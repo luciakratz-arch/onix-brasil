ccb6e80b4cfc21b34ec36ec878a152da0dd5844e
luciakratz-arch <luciakratz@gmail.com>
2026-05-17 12:09:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -126,21 +126,20 @@ var Icon = function Icon(_ref) {
     _ref$size = _ref.size,
     size = _ref$size === void 0 ? 16 : _ref$size,
     color = _ref.color;
-  var ref = React.useRef(null);
   useEffect(function () {
-    if (window.lucide && ref.current) window.lucide.createIcons({
-      icons: {},
-      rootNode: ref.current.parentNode || document.body
-    });
+    if (window.lucide) {
+      try {
+        window.lucide.createIcons();
+      } catch (e) {}
+    }
   }, [name]);
   return /*#__PURE__*/React.createElement("i", {
-    ref: ref,
     "data-lucide": name,
     style: {
       width: size,
       height: size,
       color: color || "inherit",
-      display: "block",
+      display: "inline-block",
       flexShrink: 0
     }
   });
